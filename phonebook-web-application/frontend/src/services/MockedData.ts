e385897ef495e8289adb106921af72806e683a37
Ivan Toskov <ivantoskov17@gmail.com>
2022-05-25 22:22:07 +0300

    defined the types of all declared variables and changed everything to const

--- phonebook-web-application/frontend/src/services/MockedData.ts
+++ phonebook-web-application/frontend/src/services/MockedData.ts
@@ -1,29 +1,32 @@
 import {User} from "../models/User"
 import {PhoneType} from "../models/PhoneType";
+import {PhoneNumber} from "../models/PhoneNumber";
+import {Address} from "../models/Address";
+import {Contact} from "../models/Contact";
 
-let pNum1 = {phoneNumberId: 21, phoneNumber: "0877564534", type: PhoneType.Personal};
-let pNum2 = {phoneNumberId: 22, phoneNumber: "0887675432", type: PhoneType.Personal};
-let pNum3 = {phoneNumberId: 23, phoneNumber: "0889087654", type: PhoneType.Work};
-let pNum4 = {phoneNumberId: 24, phoneNumber: "0876541209", type: PhoneType.Personal};
-let pNum5 = {phoneNumberId: 25, phoneNumber: "0888888888", type: PhoneType.Work};
-let pNum6 = {phoneNumberId: 26, phoneNumber: "6501234567", type: PhoneType.Home};
+const pNum1: PhoneNumber = {phoneNumberId: 21, phoneNumber: "0877564534", type: PhoneType.Personal};
+const pNum2: PhoneNumber = {phoneNumberId: 22, phoneNumber: "0887675432", type: PhoneType.Personal};
+const pNum3: PhoneNumber = {phoneNumberId: 23, phoneNumber: "0889087654", type: PhoneType.Work};
+const pNum4: PhoneNumber = {phoneNumberId: 24, phoneNumber: "0876541209", type: PhoneType.Personal};
+const pNum5: PhoneNumber = {phoneNumberId: 25, phoneNumber: "0888888888", type: PhoneType.Work};
+const pNum6: PhoneNumber = {phoneNumberId: 26, phoneNumber: "6501234567", type: PhoneType.Home};
 
-let addr1 = {addressId: 32, country: "Bulgaria", city: "Krumovgrad", zipCode: "6900"};
-let addr2 = {addressId: 31, country: "Bulgaria", city: "Sofia", zipCode: "1700"};
-let addr3 = {addressId: 33, country: "Bulgaria", city: "Sofia", street: "ul. Cherkovna"};
-let addr4 = {addressId: 34, country: "Bulgaria"};
-let addr5 = {addressId: 35, country: "USA"};
+const addr1: Address = {addressId: 32, country: "Bulgaria", city: "Krumovgrad", zipCode: "6900", street: undefined};
+const addr2: Address = {addressId: 31, country: "Bulgaria", city: "Sofia", zipCode: "1700", street: undefined};
+const addr3: Address = {addressId: 33, country: "Bulgaria", city: "Sofia", zipCode: undefined, street: "ul. Cherkovna"};
+const addr4: Address = {addressId: 34, country: "Bulgaria", city: undefined, zipCode: undefined, street: undefined};
+const addr5: Address = {addressId: 35, country: "USA", city: undefined, zipCode: undefined, street: undefined};
 
-let contact1 = {contactId: 41, address: addr1, name: "Ivan", phoneNumbers: [pNum1]};
-let contact2 = {contactId: 42, address: addr2, name: "Kenan", phoneNumbers: [pNum2, pNum3]};
-let contact3 = {contactId: 43, address: addr3, name: "Ivo", phoneNumbers: [pNum3]};
-let contact4 = {contactId: 44, address: addr4, name: "Evi", phoneNumbers: [pNum4, pNum5]};
-let contact5 = {contactId: 45, address: addr5, name: "George", phoneNumbers: [pNum6]};
+const contact1: Contact = {contactId: 41, address: addr1, email: undefined, name: "Ivan", photo: undefined, phoneNumbers: [pNum1]};
+const contact2: Contact = {contactId: 42, address: addr2, email: undefined, name: "Kenan", photo: undefined, phoneNumbers: [pNum2, pNum3]};
+const contact3: Contact = {contactId: 43, address: addr3, email: undefined, name: "Ivo", photo: undefined, phoneNumbers: [pNum3]};
+const contact4: Contact = {contactId: 44, address: addr4, email: undefined, name: "Evi", photo: undefined, phoneNumbers: [pNum4, pNum5]};
+const contact5: Contact = {contactId: 45, address: addr5, email: "george@gmail.com", name: "George", photo: undefined, phoneNumbers: [pNum6]};
 
-let usr1 = {userId: 11, userName: "ivantoskov", contacts: [contact2, contact3, contact4]};
-let usr2 = {userId: 12, userName: "kensevv", contacts: [contact1, contact3, contact4]};
-let usr3 = {userId: 13, userName: "ivsmars", contacts: [contact2, contact1, contact4]};
-let usr4 = {userId: 14, userName: "Ewssi", contacts: [contact2, contact3, contact1, contact5]};
-let usr5 = {userId: 15, userName: "gosho"};
+const usr1: User = {userId: 11, userName: "ivantoskov", contacts: [contact2, contact3, contact4]};
+const usr2: User = {userId: 12, userName: "kensevv", contacts: [contact1, contact3, contact4]};
+const usr3: User = {userId: 13, userName: "ivsmars", contacts: [contact2, contact1, contact4]};
+const usr4: User = {userId: 14, userName: "Ewssi", contacts: [contact2, contact3, contact1, contact5]};
+const usr5: User = {userId: 15, userName: "gosho", contacts: undefined};
 
 export const mockedUsers: User[] = [usr1, usr2, usr3, usr4, usr5];
