4cf278d0ccd5541aeeb8083763f8120d0d6232a7
Ewssi <evelina29300@abv.bg>
2022-06-10 17:40:35 +0300

    added contact class

--- phonebook-web-application/backend/src/main/java/fmi/web/backend/entity/Contact.java
+++ phonebook-web-application/backend/src/main/java/fmi/web/backend/entity/Contact.java
@@ -1,4 +1,27 @@
 package fmi.web.backend.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.sql.Blob;
+import java.util.List;
+
+@Entity
 public class Contact {
+
+    @Id
+    private String contactUuId;
+
+
+    private Address address;
+
+    private String email;
+
+    private String name;
+
+    private Blob photo;
+
+    @ManyToOne
+    private List<PhoneNumber> phoneNumbers;
+
 }
